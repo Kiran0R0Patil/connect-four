2848e05cf24416865437ac68bd7d192752913339
Kiran R Patil <91089009+Kiran0R0Patil@users.noreply.github.com>
2022-05-05 22:53:06 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => 
+{
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.getElementById('result')
+    const displayCurrentPlayer = document.getElementById('current-player')
+    let currentPlayer = 1
+
+})
